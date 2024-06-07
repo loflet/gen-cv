ca0f66a328fc095e0cd96e7dce93de228fcf18bc
Sanket Ghorpade <ghorpade.sanket@hotmail.com>
2024-06-07 17:17:52 +0200

    Changed init file

--- avatar/interactive/api/message/__init__.py
+++ avatar/interactive/api/message/__init__.py
@@ -385,8 +385,8 @@ def get_product_information(user_question, categories='*', top_k=1):
 def chat_complete(messages, functions, function_call='auto'):
     """  Return assistant chat response based on user query. Assumes existing list of messages """
     
-    url = f"{AOAI_endpoint}/openai/deployments/{chat_deployment}/chat/completions?api-version={AOAI_api_version}"
-
+    url = f"{AOAI_endpoint}/openai/deployments/{chat_deployment}/completions?api-version={AOAI_api_version}"
+    logging.info(url)
     headers = {
         "Content-Type": "application/json",
         "api-key": AOAI_key
